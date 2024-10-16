814d86c1b0971d9d31884b7e76e1a97fdb0c7f5a
minhnda-se <anhminh02111@gmail.com>
2024-10-16 23:21:13 +0700

    Version 0.2.1 Fetures: Create Ui for koi detail, add new koi Select image from local Updates: Update UI for window form Update UX Fix-bug: Adjust when the app is scaled

--- KoiCareSystemAtHome-App/KoiCareSystemAtHome-App/KoiWindow.xaml.cs
+++ KoiCareSystemAtHome-App/KoiCareSystemAtHome-App/KoiWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace KoiCareSystemAtHome_App
 {
@@ -55,7 +56,88 @@ namespace KoiCareSystemAtHome_App
             }
 
         }
+        private void ChangeUIWhenClickButton(Button clickButton, Button button, Button button1)
+        {
+            clickButton.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#db4b2e"));
+            button.BorderBrush = Brushes.Transparent;
+            button1.BorderBrush = Brushes.Transparent;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            if (clickedButton == btn_add || clickedButton == btn_add1)
+            {
+                ChangeUIWhenClickButton(btn_add, btn_detail, btn_list);
+                KoiDetailGrid.Visibility = Visibility.Collapsed;
+                KoiDataGrid.Visibility = Visibility.Collapsed;
+                NewKoiEntryGrid.Visibility = Visibility.Visible;
+            }
+            else if (clickedButton == btn_detail)
+            {
+                ChangeUIWhenClickButton(btn_detail, btn_add, btn_list);
+                KoiDataGrid.Visibility = Visibility.Collapsed;
+                NewKoiEntryGrid.Visibility = Visibility.Collapsed;
+                KoiDetailGrid.Visibility = Visibility.Visible;
+            }
+            else if (clickedButton == btn_list)
+            {
+                ChangeUIWhenClickButton(btn_list, btn_detail, btn_add);
+                KoiDetailGrid.Visibility = Visibility.Collapsed;
+                NewKoiEntryGrid.Visibility = Visibility.Collapsed;
+                KoiDataGrid.Visibility = Visibility.Visible;
+            }
+        }
 
+        private void SelectImg_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp|All Files|*.*",
+                Title = "Select a Koi Image"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Set the selected image to the Image control
+                KoiImage.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+            }
+            else
+            {
+                
+                KoiImage.Source = null; 
+            }
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Logout_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AddKoi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void CancelAddKoi_Click(Object sender, RoutedEventArgs e)
+        {
+
+            KoiImage.Source = null;
+        }
+
+        private void NumberOnly(object sender, TextCompositionEventArgs e)
+        {
+            // Check if the input is a digit
+            e.Handled = !int.TryParse(e.Text, out _);
+        }
+
+        private void EditKoi_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 
 }
